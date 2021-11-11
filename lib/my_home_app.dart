8046815f32aa3fe2308ba01ebd9f2292ea32e994
Ilya Kiselev <ruilyakiselev@gmail.com>
2021-11-11 15:08:34 +0700

    learning how to integrate scrollview (for working with portrait and landscape mode correctly); add floating button with label, add some container with style

--- lib/my_home_app.dart
+++ lib/my_home_app.dart
@@ -23,47 +23,97 @@ class _MyHomeAppState extends State<MyHomeApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.white,
           appBar: AppBar(
               title: const Text("Some title"),
               backgroundColor: Colors.green
           ),
           body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "$_counter",
-                    key: ValueKey<int>(_counter),
-                    style: const TextStyle(
-                      fontSize: 50,
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () => {
-                      increment(),
-                      Fluttertoast.showToast(
-                          msg: "new event",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.CENTER,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.red,
-                          textColor: Colors.white,
-                          fontSize: 16.0
-                      )
-                    },
-                    child: const Text("Press Button"),
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        textStyle: const TextStyle(
-                            fontSize: 30
+              child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "$_counter",
+                        key: ValueKey<int>(_counter),
+                        style: const TextStyle(
+                          fontSize: 50,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
                         ),
-                        primary: Colors.white
-                    ),
+                      ),
+                      TextButton(
+                        onPressed: () => {
+                          increment(),
+                          Fluttertoast.showToast(
+                              msg: "new event",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.CENTER,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.red,
+                              textColor: Colors.white,
+                              fontSize: 16.0
+                          )
+                        },
+                        child: const Text("Press Button"),
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            textStyle: const TextStyle(
+                                fontSize: 30
+                            ),
+                            primary: Colors.white
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () => {
+                            increment(),
+                            Fluttertoast.showToast(
+                                msg: "new event",
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0
+                            )
+                          },
+                          icon: const Icon(Icons.add),
+                          splashColor: Colors.red,
+                          padding: const EdgeInsets.all(20)
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        margin: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(10.0)
+                        ),
+                        child: const Center(
+                          child: Text("aaaaa")
+                        ),
+                      )
+                    ],
                   )
-                ],
               )
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+            label: const Text("Label"),
+            onPressed: () => {
+              increment(),
+              Fluttertoast.showToast(
+                  msg: "new event",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0
+              )
+            },
+            icon: const Icon(Icons.add),
+            tooltip: "some description",
           ),
         )
     );
